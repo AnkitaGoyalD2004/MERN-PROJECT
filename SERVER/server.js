66ae6f31e0e368219372eb10e2387fe6544008e9
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-05-24 11:06:40 +0530

    add middleware

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
+
+//use of Middleware
+app.use(express.json());
+//This line of code adds Express middleware that parses incoming request bodies with JSON payloads . It's important to place this before any routes that need to handle JSON data in the req body . This middleware is responsible for parsing JSON data from requests , and it should be applied at the beganing of your middleware stack to ensure it's available for all subsequent route handler.
+
 //Mount the router: To use the router in your main Express , you cannot "mount" it at a specific URL prefix
 
 //
