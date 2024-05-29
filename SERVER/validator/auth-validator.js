2a2957d743054218ba3e8599f95119226dd48e83
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-05-29 09:58:30 +0530

    Schema validation

--- SERVER/validator/auth-validator.js
+++ SERVER/validator/auth-validator.js
@@ -1,3 +1,21 @@
-const { z } = require("yup");
+const yup = require("yup");
 
 //Creating an object schema
+
+const userSchema = yup.object().shape({
+  username: yup.string().required("Name is required"),
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  age: yup
+    .number()
+    .positive("Age must be positive")
+    .integer("Age must be an integer")
+    .min(18, "Must be 18 or older"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters long")
+    .required("Password is required"),
+  // Add more fields and validation rules as needed
+});
